6423ef1b10d3d6e27c117afe52cf771cd2b94116
Allyhere <camilomichelettors@gmail.com>
2025-08-15 22:27:09 -0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { Footer } from "./components/Footer/Footer";
-import { NavbarItems } from "./components/Navbar/NavbarItems";
-import { NavbarWrapper } from "./components/Navbar/NavbarWrapper";
-import { Integrantes } from "./pages/Integrantes/Integrantes";
 function App() {
   return (
     <>
-      <NavbarWrapper>
-        <NavbarItems />
-      </NavbarWrapper>
-      <Integrantes />
-      <Footer />
+      <h1>Página principal</h1>
     </>
   );
 }
